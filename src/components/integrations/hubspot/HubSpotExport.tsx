8b613f165d691c86ab44f0d3d2118e8238d63944
Thierry Ntirandekura <thierryntirandekura@Thierrys-MacBook-Pro.local>
2025-04-17 18:03:15 +0200

    syles: removing border bottom as separator on the search input and list of properties from hubspot

--- src/components/integrations/hubspot/HubSpotExport.tsx
+++ src/components/integrations/hubspot/HubSpotExport.tsx
@@ -379,9 +379,9 @@ const HubSpotExport: React.FC<HubSpotExportProps> = ({
 
               {/* HubSpot Properties Modal */}
               {showHubSpotProperties && (
-                <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+                <div className="p-4 fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
                   <div className="bg-white rounded-lg shadow-xl w-full max-w-2xl max-h-[80vh] flex flex-col">
-                    <div className="p-4 border-b border-gray-200">
+                    <div className="p-4">
                       <div className="flex justify-between items-center">
                         <h3 className="text-lg font-medium text-gray-900">
                           Map to HubSpot Property
