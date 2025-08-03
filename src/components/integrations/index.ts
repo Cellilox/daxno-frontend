886e043cb02ec854ee58f1f2a66e57677ab0703a
Thierry Ntirandekura <pro_thierry@Thierrys-MacBook-Pro-2.local>
2025-08-03 13:31:56 +0200

    fix: put Integrations exported from index.ts

--- src/components/integrations/index.ts
+++ src/components/integrations/index.ts
@@ -1,4 +1,5 @@
 export { GoogleDriveIcon, GoogleSheetsIcon } from './google-drive/GoogleDriveIcons';
 export { default as GoogleDriveExport } from './google-drive/GoogleDriveExport';
 export { default as DownLoadCSV } from './DownlaodCSV';
+export { default as Integrations } from './Integrations';
 export * from './types'; 
